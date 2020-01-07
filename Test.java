e348d781088ca694da147583cff202c07f778967
Dania Anwar <daniaaa@MacBook-Pro-3.local>
2020-01-07 16:40:16 +0200

    added a dania person

--- Test.java
+++ Test.java
@@ -3,25 +3,27 @@ public class Test
     public static void main (String[] args)
     {
         System.out.println ("Testing git at PSUT");
-        
+
         int a;
         int b;
         float result;
-        
+
         a= 10;
         b= 3;
         result= (float) a / b;
-        
+
         System.out.println ("result= " + result);
-        
+
         Person p= new Person ("Mary", 4);
         System.out.println (p.getName ());
-		
+
         Person p= new Person ("Yaza", 34);
-        System.out.println (p.getName ());		
+        System.out.println (p.getName ());
 
         Person p= new Person ("Lobo", 33);
-        System.out.println (p.getName ());		
+        System.out.println (p.getName ());
 
+        Person p= new Person ("Dania", 17);
+        System.out.println (p.getName ());
     }
 }
